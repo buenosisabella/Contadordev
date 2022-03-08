1540de1428ae6697dd7844a10bb6ad97cdce2796
buenosisabella <coreamilisabella@gmail.com>
2022-03-08 00:51:59 -0300

    sucesso

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Div = styled.div`
+height: 100vh;
+color: #ffffff;
+display: flex;
+justify-content: center;
+align-items: center; 
+flex-direction: column;
+font-size: 2.5rem;
+text-shadow: #fff x;
+background: linear-gradient(to right, #13ac99, #101290);
+
+`;
+const H1 = styled.h1`
+font-size: 2.5rem;
+` 
+
+const Button = styled.button`
+height: 3rem;
+width: 3rem;
+margin: 2ren;
+font-size: 2rem;
+border-radius: 50px;
+background: transparent ; 
+`
+class Contador extends Component{
+  state= {
+    count:0
+  };
+
+  add = () => {
+    if (this.state.count < 10) {
+      this.setState({
+        count: this.state.
+          count + 1
+      });
+    }
+  };
+
+  remove = () => {
+    if (this.state.count > 0) {
+      this.setState({
+        count: this.state.
+          count - 1
+      });
+    }
+  };
+
+  render() {
+    return (
+      <Div>
+        <H1>Contador</H1>
+        <Button onClick={this.add}>+</Button>
+        <h2>{this.state.count}</h2>
+        <Button onClick={this.remove}>-</Button>
+      </Div>
+    )
+  }
 }
 
-export default App;
+export default Contador;
